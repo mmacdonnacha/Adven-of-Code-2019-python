fa8409d4aba8d63452fb593a94fc03f87eee186d
Micheal Mac Donnacha <micheal.macdonnacha13@gmail.com>
2020-02-27 00:36:13 +0000

    adding completed day02

--- day02/day02.py
+++ day02/day02.py
@@ -11,6 +11,7 @@
 
 def part1(intcode):
     intcode = [int(i) for i in intcode]
+    
     for index in range(0, len(intcode), 4):
         opcode = intcode[index]
         if opcode == 1:
@@ -28,6 +29,15 @@ def part1(intcode):
     
     return intcode
 
+def part2(intcode):
+    for noun in range(0, 100):
+        for verb in range(0, 100):
+            intcode[1] = noun
+            intcode[2] = verb
+
+            if(part1(intcode)[0] == 19690720):
+                return noun, verb
+
 
 def solve():
     part1_answer = 0
@@ -41,11 +51,13 @@ def solve():
         part1_list[2] = 2
         part1_answer = part1(part1_list)
 
+        part2_list = list.copy(intcode)
+        part2_answer = part2(part2_list)
+
     print(f'Part 1: {part1_answer[0]}')
+    print(f'Part 1: {100 * part2_answer[0] + part2_answer[1]}')
 
 
 
 if __name__ in '__main__':
     solve()
-
-
