4dc2f020fade80bc9fb98cdeec3f88ad0be3f717
Micheal Mac Donnacha <micheal.macdonnacha13@gmail.com>
2020-02-28 20:00:32 +0000

    removing some of the commented code in day03.py

--- day03/day03.py
+++ day03/day03.py
@@ -6,16 +6,10 @@ def part1(first_path, second_path):
     for direction in first_path:
         first_path_list = update_path_part1(first_path_list, direction)
 
-    # print('First path created')
-
     for direction in second_path:
         second_path_list = update_path_part1(second_path_list, direction)
 
-    # print('Second path created')
-
     common_points = common_elements(first_path_list, second_path_list)
-
-    # print('Common points created')
 
     shortest_distance = 0
     for point in common_points:
@@ -36,16 +30,11 @@ def part2(first_path, second_path):
     for direction in first_path:
         first_path_list, steps_first = update_path_part2(first_path_list, direction, steps_first)
 
-    print('First path created')
-
     for direction in second_path:
         second_path_list, steps_second = update_path_part2(second_path_list, direction, steps_second)
 
-    print('Second path created')
-
     common_points = common_elements(first_path_list, second_path_list)
 
-    print('Common points created')
     shortest_distance = 0
     
     for point in common_points:
@@ -68,28 +57,24 @@ def update_path_part1(direction_list, direction):
             current_place = (starting_point[0] + x, starting_point[1])
             if current_place not in direction_list:
                 direction_list.append(current_place)
-            # print(current_place)
-    
+            
     if arrow is 'L':
         for x in range(1, length+1):
             current_place = (starting_point[0] - x, starting_point[1])
             if current_place not in direction_list:
                 direction_list.append(current_place)
-            # print(current_place)
-            
+           
     if arrow is 'U':
         for y in range(1, length+1):
             current_place = (starting_point[0], starting_point[1] + y)
             if current_place not in direction_list:
                 direction_list.append(current_place)
-            # print(current_place)
-
+            
     if arrow is 'D':
         for y in range(1, length+1):
             current_place = (starting_point[0], starting_point[1] - y)
             if current_place not in direction_list:
                 direction_list.append(current_place)
-            # print(current_place)
 
     return direction_list
 
@@ -97,7 +82,6 @@ def update_path_part2(direction_list, direction, steps_second):
     arrow = direction[:1]
     length = int(direction[1:])
     starting_point = direction_list[-1]
-    # steps = direction_list[-1][1]
     steps = steps_second
     i = steps[-1]
 
@@ -151,9 +135,7 @@ def solve_part1():
             
         part1_answer = part1(paths[0], paths[1])
         
-
     print(f'Part 1: {part1_answer}')
-    # print(f'Part 1: {100 * part2_answer[0] + part2_answer[1]}')
 
 def solve_part2():
     
@@ -167,14 +149,11 @@ def solve_part2():
         
 
     print(f'Part 2: {answer}')
-    # print(f'Part 1: {100 * part2_answer[0] + part2_answer[1]}')
-
 
 
 if __name__ in '__main__':
     # solve_part1()
     solve_part2()
-    # print(part2( ['R8', 'U5', 'L5', 'D3'], ['U7', 'R6', 'D4', 'L4'] ))
 
 # need to refactor at some stange 
 # very slow when using the input file
